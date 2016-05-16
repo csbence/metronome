4b2800015830fa6b0969f7781ed631936e93c680
Skatt-unh <sammie.katt@gmail.com>
2016-05-16 17:21:04 -0400

    formatted priority correctly

--- test/util/PriorityQueueTest.hpp
+++ test/util/PriorityQueueTest.hpp
@@ -1,26 +1,20 @@
 #define CATCH_CONFIG_COLOUR_NONE
 
-#include "../../src/util/PriorityQueue.hpp"
-#include "../dependencies/catch.hpp"
+#include "util/PriorityQueue.hpp"
+#include "catch.hpp"
 #include "easylogging++.h"
 
-INITIALIZE_EASYLOGGINGPP
-
 namespace {
-class TestNode {
- public:
 
-};
+class TestNode {};
 
-int nodeCompare(const TestNode&, const TestNode&) {
-    return 1;  // TODO: actually compare test nodes
+int nodeCompare(const TestNode &, const TestNode &) {
+  return 1; // TODO: actually compare test nodes
 }
 
 auto queue = PriorityQueue<TestNode>(100, nodeCompare);
 
-
 TEST_CASE("PriorityQueue test", "[PriorityQueue]") {
-    REQUIRE(queue.getCapacity() == 100); 
+  REQUIRE(queue.getCapacity() == 100);
 }
-
 }
