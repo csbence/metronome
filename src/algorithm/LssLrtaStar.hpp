c1e2c8c1c3d01bea22e1eaeaa4bd88476f27d6d0
Bence Cserna <bence@cserna.net>
2016-07-02 00:09:43 -0400

    Add learning and exploration to LSS-LRTA*

--- src/algorithm/LssLrtaStar.hpp
+++ src/algorithm/LssLrtaStar.hpp
@@ -1,6 +1,7 @@
 #ifndef METRONOME_LSSLRTASTAR_HPP
 #define METRONOME_LSSLRTASTAR_HPP
 #include <boost/pool/object_pool.hpp>
+#include <fcntl.h>
 #include <unordered_map>
 #include <util/Hasher.hpp>
 #include <util/PriorityQueue.hpp>
@@ -36,21 +37,18 @@ class LssLrtaStar {
     }
 
 private:
-    class Edge {
-    public:
-        Edge(Node* predecessor, Action action, Cost actionCost)
-                : predecessor{predecessor}, action{action}, actionCost{actionCost} {
-        }
-
-        const Node* predecessor;
-        const Action action;
-        const Cost actionCost;
-    };
+    class Edge;
 
     class Node {
     public:
-        Node(Node* parent, const State& state, Action action, Cost g, Cost h, bool open)
-                : parent{parent}, state{state}, action{std::move(action)}, g{g}, h{h}, open{open} {
+        Node(Node* parent, const State& state, Action action, Cost g, Cost h, bool open, unsigned int iteration = 0)
+                : parent{parent},
+                  state{state},
+                  action{std::move(action)},
+                  g{g},
+                  h{h},
+                  open{open},
+                  iteration{iteration} {
         }
 
         unsigned long hash() {
@@ -76,28 +74,132 @@ private:
         /** True if the node is in the open list */
         bool open;
         /** Last iteration when the node was updated */
-        unsigned int iteration{0};
+        unsigned int iteration;
         /** List of all the predecessors that were discovered in the current exploration phase. */
         std::vector<Edge> predecessors;
     };
 
+    class Edge {
+    public:
+        Edge(Node* predecessor, Action action, Cost actionCost)
+                : predecessor{predecessor}, action{action}, actionCost{actionCost} {
+        }
+
+        const Node* predecessor;
+        const Action action;
+        const Cost actionCost;
+    };
+
     void learn() {
-        //TODO
+        ++iterationCounter;
+
+        // Reorder the open list based on the heuristic values
+        openList.reorder(hValueComparator);
+
+        while (/* check termination && */ openList.isNotEmpty()) {
+            auto currentNode = popOpenList();
+            currentNode->iteration = iterationCounter;
+
+            Cost currentHeuristicValue = currentNode->h;
+
+            // update heuristic value of each predecessor
+            for (auto predecessor : currentNode->predecessors) {
+                Node* predecessorNode = predecessor.node;
+
+                if (predecessorNode->iteration == iterationCounter && !predecessorNode->open) {
+                    // This node was already learned and closed in the current iteration
+                    continue;
+                    // TODO Review this. This could be incorrect if the action costs are not uniform
+                }
+
+                if (!predecessorNode->open) {
+                    // This node is not open yet, because it was not visited in the current planning iteration
+
+                    predecessorNode->h = currentHeuristicValue + predecessor.actionCost;
+                    assert(predecessorNode->iteration == iterationCounter - 1);
+                    predecessorNode->iteration = iterationCounter;
+
+                    addToOpenList(*predecessorNode);
+                } else if (predecessorNode->h > currentHeuristicValue + predecessor.actionCost) {
+                    // This node was visited in this learning phase, but the current path is better then the previous
+                    predecessorNode->h = currentHeuristicValue + predecessor.actionCost;
+                    openList.update(*predecessorNode);
+                }
+            }
+        }
     }
 
-    void explore(const State& startState) {
+    Node* explore(const State& startState) {
         ++iterationCounter;
         clearOpenList();
         openList.reorder(fValueComparator);
 
         const Node localStartNode =
-            Node(nullptr, std::move(startState), Action(-1), 0, domain.heuristic(startState), true);
+                Node(nullptr, std::move(startState), Action(-1), 0, domain.heuristic(startState), true);
 
         auto startNode = nodePool.construct(localStartNode);
+        Node* currentNode = startNode;
 
         nodes[startNode->state] = startNode;
+        addToOpenList(*startNode);
 
-        // TODO finish implementation
+        while (/* check termination && */ !domain.isGoal(currentNode->state)) {
+            currentNode = popOpenList();
+            expandNode(currentNode);
+        }
+
+        return currentNode;
+    }
+
+    void expandNode(Node* sourceNode) {
+        // TODO increase node count
+
+        auto currentGValue = sourceNode->g;
+        for (auto successor : domain.successors(sourceNode->state)) {
+            auto successorState = successor.state;
+
+            Node*& successorNode = nodes[successorState];
+
+            if (successorNode == nullptr) {
+                // TODO increment generated node count
+
+                const Node tempSuccessorNode(
+                        sourceNode, successor.state, successor.action, successor.actionCost, domain.COST_MAX, true);
+
+                successorNode = nodePool.construct(std::move(tempSuccessorNode));
+            }
+
+            // If the node is outdated it should be updated.
+            if (successorNode->iteration != iterationCounter) {
+                successorNode->iteration = iterationCounter;
+                successorNode->predecessors.clear();
+                successorNode->g = domain.COST_MAX;
+                successorNode->open = false; // It is not on the open list yet, but it will be
+                // parent, action, and actionCost is outdated too, but not relevant.
+            }
+
+            // Add the current state as the predecessor of the child state
+            successorNode->predecessors.emplace_back(sourceNode, successor.action, successor.actionCost);
+
+            // Skip if we got back to the parent
+            if (successorState == sourceNode->parent->state) {
+                continue;
+            }
+
+            // only generate those state that are not visited yet or whose cost value are lower than this path
+            Cost successorGValueFromCurrent{currentGValue + successor.actionCost};
+            if (successorNode->g > successorGValueFromCurrent) {
+                successorNode->g = successorGValueFromCurrent;
+                successorNode->parent = sourceNode;
+                successorNode->action = successor.action;
+
+                if (!successorNode->open) {
+                    addToOpenList(*successorNode);
+                } else {
+                    openList.update(*successorNode);
+                }
+            }
+        }
     }
 
     void clearOpenList() {
