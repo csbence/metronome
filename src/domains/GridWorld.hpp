965e052ccaaf749d58d14638d3963f49c8059182
Will Doyle <doylew@newyork6.org>
2016-07-15 13:42:30 -0400

    updated GridWorld to have a parsing constructor

--- src/domains/GridWorld.hpp
+++ src/domains/GridWorld.hpp
@@ -144,11 +144,43 @@ private:
 
 public:
     GridWorld(const Configuration& config, std::fstream& input) {
-        this->startLocation = State::newState(0, 0, 0);
-        this->goalLocation = State::newState(4, 4, 0);
-        this->width = 5;
-        this->height = 5;
         this->blockedCells = std::vector<State>{};
+        int currentHeight = 0;
+        int currentWidth = 0;
+        std::string line;
+        if (input.is_open()) {
+          getline(input, line); // get the height
+          std::stringstream convertHeight(line); 
+          convertHeight >> this->height;
+          getline(input, line); //get the width
+          std::stringstream convertWidth(line);
+          convertWidth >> this->width;
+          while ( getline (input, line) ) {
+            for ( auto it = line.cbegin(); it != line.cend(); ++it) {
+              if(*it == '@') { // find the start location
+                this->startLocation = State::newState(currentWidth, currentHeight, 0);
+              }
+              else if(*it == '*') {  //find the goal location
+                this->goalLocation = State::newState(currentWidth, currentHeight, 0); 
+              }
+              else if(*it == '#') { //store the objects
+                State object = State::newState(currentWidth,currentHeight, 0);
+                this->blockedCells.push_back(object);
+              }
+              else {
+                //its an open cell nothing needs to be done
+              }
+              ++currentWidth; // at the end of the character parse move along
+            } 
+            currentWidth = 0; //restart character parse at beginning of line
+            ++currentHeight; //move down one line in charadter parse
+          }
+          input.close();
+        }    
+        //this->startLocation = State::newState(0, 0, 0);
+        //this->goalLocation = State::newState(4, 4, 0);
+        //this->width = 5;
+        //this->height = 5;
     }
     GridWorld(State start = State::newState(0, 0, 0), State goal = State::newState(4, 4, 0), unsigned int width = 5,
             unsigned int height = 5, std::vector<State> objectStates = std::vector<State>{})
