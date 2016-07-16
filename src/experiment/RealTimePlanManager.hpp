373830bb619c0b85d1bff8b880773f305fe9ad0c
Bence Cserna <bence@cserna.net>
2016-07-16 09:39:21 -0400

    Add RT plan manager implementation

--- src/experiment/RealTimePlanManager.hpp
+++ src/experiment/RealTimePlanManager.hpp
@@ -9,16 +9,52 @@ template <typename Domain, typename Planner>
 class RealTimePlanManager : PlanManager<Domain, Planner> {
 public:
     Result plan(const Configuration& configuration, const Domain& domain, Planner& planner) {
-        auto executionTime = measureNanoTime([&] {
+        std::vector<typename Domain::Action> actions;
+        long long int planningTime{0};
+        Domain::State currentState = domain.getStartState();
 
-            // TODO start state
-            //            planner.plan(configuration);
+        while (!domain.isGoal(currentState)) {
+            auto planningIterationTime = measureNanoTime([&] {
+                auto actionList{planner.plan(currentState)};
 
-        });
+                for (auto& action : actionList) {
+                    currentState = domain.transition(currentState, action);
+                    actions.emplace_back(action);
+                }
 
-        //        return Result();
-        throw MetronomeException("RTPlanManager is not implemented");
+            });
+
+            // Increase the total planning time after each planning iteration
+            planningTime += planningIterationTime;
+        }
+
+        auto pathLength = actions.size();
+
+        std::vector<std::string> actionStrings;
+
+        for (auto& action : actions) {
+            actionStrings.push_back(action.toString());
+        }
+
+        return Result(configuration,
+                planner.getExpandedNodeCount(),
+                planner.getGeneratedNodeCount(),
+                planningTime,
+                pathLength * configuration.getLong("actionDuration"),
+                planningTime + pathLength * configuration.getLong("actionDuration"),
+                planningTime,
+                pathLength,
+                actionStrings);
     }
+};
+
+template <typename Domain>
+class ActionBundle {
+public:
+    ActionBundle(Domain::Action action, Domain::Cost cost) : action{action}, cost{cost} {
+    }
+    const Domain::Action action;
+    const Domain::Cost cost;
 };
 }
 
