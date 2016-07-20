683d449e4f5ca1d98191cf3fef1221d36c01685d
Bence Cserna <bence@cserna.net>
2016-07-20 10:35:29 -0400

    Add TimeTerminationChecker implementation

--- src/experiment/termination/TimeTerminationChecker.hpp
+++ src/experiment/termination/TimeTerminationChecker.hpp
@@ -1,13 +1,29 @@
 #ifndef METRONOME_TIMETERMINATIONCHECKER_HPP
 #define METRONOME_TIMETERMINATIONCHECKER_HPP
 
+#include <chrono>
+
 namespace metronome {
 
 class TimeTerminationChecker {
 public:
-    bool reachedTermination() {
-        return false; // TODO Add implementation
+    void resetTo(std::chrono::nanoseconds timeLimit) {
+        this->timeLimit = timeLimit;
+        startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    bool reachedTermination() const {
+        std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::nanoseconds> currentTime =
+                std::chrono::high_resolution_clock::now();
+
+        std::chrono::nanoseconds ellapsedTime = currentTime - startTime;
+
+        return ellapsedTime >= timeLimit;
     };
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::nanoseconds> startTime;
+    std::chrono::nanoseconds timeLimit;
 };
 }
 
