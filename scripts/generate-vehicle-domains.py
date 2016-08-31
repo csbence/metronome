a2c95d061981882ea668f737978fe1d9a25e78c4
Will Doyle <doylew@newyork6.org>
2016-08-31 13:49:06 -0400

    added script to generate vehicle domains

---
+++ scripts/generate-vehicle-domains.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+import os.path
+import argparse 
+import random
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("height", help="the height of the Vehicle world")
+parser.add_argument("width", help="the width of the Vehicle world")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-n", "--number", help="number of worlds to generate")
+
+args = parser.parse_args()
+height = int(args.height)
+width = int(args.height)
+number = int(args.number)
+
+if args.verbose:
+  print(args.height)
+  print(args.width)
+
+obstaclePercentage = 0.30
+bunkerPercentage = 0.30
+startX = 1
+startY = 1
+
+endX = width
+endY = height
+
+path = "../resources/input/vehicle"
+
+for iteration in range(0,number):
+
+  newDomain = "vehicle"+str(iteration)
+
+  completeFile = os.path.join(path, newDomain+".v")
+
+  aFile = open(completeFile, "w")
+
+  preamble = args.height+"\n"+args.width+"\n"
+  world = ""
+
+  for y in range(1,height):
+    for x in range(1,width):
+      flipObstacle = random.random()
+      flipBunker = random.random() 
+      if ((x == startX) and (y == startY)):
+        world += "@"
+      elif flipObstacle < obstaclePercentage:
+        world += "#"
+      elif flipBunker < bunkerPercentage:
+        world += "$"
+      else:
+        world += "_"
+    world += "\n"
+    
+  aFile.write(preamble + world)
+
+  aFile.close()
