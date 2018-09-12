ccc1fd8fd24aa29f2b94b55117f9edcd6a4d0a1d
Bence Cserna <bence@cserna.net>
2018-09-12 01:18:11 -0400

    Add .empty() to ObjectPool

--- src/utils/ObjectPool.hpp
+++ src/utils/ObjectPool.hpp
@@ -7,43 +7,48 @@ namespace metronome {
 
 template <typename T, std::size_t N>
 class ObjectPool {
-    using Storage = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
-public:
-    ObjectPool() : storage(new Storage[N]) {}
+  using Storage = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
 
-    ObjectPool(const ObjectPool&) = delete;
+ public:
+  ObjectPool() : storage(new Storage[N]) {}
 
-    ~ObjectPool() {
-        purge();
-        delete[] storage;
+  ObjectPool(const ObjectPool&) = delete;
+
+  ~ObjectPool() {
+    purge();
+    delete[] storage;
+  }
+
+  // Create an object in aligned storage
+  template <typename... Args>
+  T* construct(Args&&... args) {
+    if (size >= N)  // possible error handling
+      throw std::bad_alloc{};
+    T* const t = new (storage + size) T(std::forward<Args>(args)...);
+    ++size;
+
+    return t;
+  }
+
+  void purge() {
+    for (std::size_t i = 0; i < size; ++i) {
+      reinterpret_cast<const T*>(storage + i)->~T();
     }
 
-    // Create an object in aligned storage
-    template <typename... Args>
-    T* construct(Args&&... args) {
-        if (size >= N) // possible error handling
-            throw std::bad_alloc{};
-        T* const t = new (storage + size) T(std::forward<Args>(args)...);
-        ++size;
+    delete[] storage;
+  }
 
-        return t;
-    }
+  // Access an object in aligned storage
+  T* operator[](std::size_t index) const {
+    return reinterpret_cast<const T*>(storage + index);
+  }
 
-    void purge() {
-        for (std::size_t i = 0; i < size; ++i) {
-            reinterpret_cast<const T*>(storage + i)->~T();
-        }
+  bool empty() const { return size == 0; }
 
-        delete[] storage;
-    }
-
-    // Access an object in aligned storage
-    T* operator[](std::size_t index) const { return reinterpret_cast<const T*>(storage + index); }
-
-private:
-    Storage* storage;
-    std::size_t size = 0;
+ private:
+  Storage* storage;
+  std::size_t size = 0;
 };
-}
+}  // namespace metronome
 
-#endif // METRONOME_LINEARMEMORYPOOL_HPP
+#endif  // METRONOME_LINEARMEMORYPOOL_HPP
