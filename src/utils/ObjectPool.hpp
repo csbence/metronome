9400b3d4e3a8ccbb88934c2d4bfa4e725b2a0fd7
Bence Cserna <bence@cserna.net>
2018-09-17 12:38:41 -0400

    Add recycling to ObjectPool

--- src/utils/ObjectPool.hpp
+++ src/utils/ObjectPool.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdio>
 #include <memory>
+#include <bitset>
+
 namespace metronome {
 
 template <typename T, std::size_t N>
@@ -9,7 +11,9 @@ class ObjectPool {
   using Storage = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
 
  public:
-  ObjectPool() : storage(new Storage[N]) {}
+  ObjectPool() : storage(new Storage[N]), freeIndices(), size(0) {
+    freeIndices.flip();
+  }
 
   ObjectPool(const ObjectPool&) = delete;
 
@@ -24,19 +28,31 @@ class ObjectPool {
   T* construct(Args&&... args) {
     if (size >= N)  // possible error handling
       throw std::bad_alloc{};
-    T* const t = new (storage + size) T(std::forward<Args>(args)...);
+    
+    std::size_t freeIndex = getFreeIndex();
+    T* const t = new (storage + freeIndex) T(std::forward<Args>(args)...);
+    freeIndices[freeIndex] = false;
     ++size;
 
     return t;
   }
 
+  void destruct(T* item) {
+    freeIndices[index(item)] = true;
+    --size;
+    item->~T();
+  }
+  
   void purge() {
     for (std::size_t i = 0; i < size; ++i) {
       reinterpret_cast<const T*>(storage + i)->~T();
+      freeIndices[i] = true;
     }
+    
+    size = 0;
+    lastFreeIndex = 0;
   }
 
-  // Access an object in aligned storage
   T* operator[](std::size_t index) const {
     return reinterpret_cast<const T*>(storage + index);
   }
@@ -44,12 +60,30 @@ class ObjectPool {
   std::size_t index(const T* item) const {
     return static_cast<std::size_t>(item - reinterpret_cast<T*>(storage));
   }
-
+  
   bool empty() const { return size == 0; }
 
  private:
+  std::size_t getFreeIndex() {
+    for (std::size_t i = lastFreeIndex; i < N; ++i) {
+      if (freeIndices[i]) {
+        lastFreeIndex = i;
+        return i;
+      }
+    }
+
+    for (std::size_t i = 0; i < N; ++i) {
+      if (freeIndices[i]) {
+        lastFreeIndex = i;
+        return i;
+      }
+    }
+  }
+  
   Storage* storage;
-  std::size_t size = 0;
+  std::bitset<N> freeIndices;
+  std::size_t size;
+  std::size_t lastFreeIndex;
 };
 
 }  // namespace metronome
